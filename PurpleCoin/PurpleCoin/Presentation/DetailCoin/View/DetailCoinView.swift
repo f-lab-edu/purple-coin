aee0c616aa8e74f9ada8c41ce021f740099fcb5c
kang-jae-heok <kjhabc8707@naver.com>
2024-01-04 22:51:17 +0900

    chore: 리소스 추가 back, star, star_filled 추가

--- PurpleCoin/PurpleCoin/Presentation/DetailCoin/View/DetailCoinView.swift
+++ PurpleCoin/PurpleCoin/Presentation/DetailCoin/View/DetailCoinView.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class DetailCoinView: UIView {
     
+    private enum Font {
+        static let topTitleLabelFont = PurpleCoinFont.font(type: .bold, size: 20)
+    }
+    
+    //MARK: topSection
+    let topView: UIView = {
+        let view = UIView()
+        return view
+    }()
+    let backButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "back"), for: .normal)
+        return button
+    }()
+    let topTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "도지코인"
+        label.textColor = .white
+        label.font = Font.topTitleLabelFont
+        return label
+    }()
+    let interestButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "star"), for: .normal)
+        button.setImage(UIImage(named: "star_filled"), for: .selected)
+        return button
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+//MARK: Layout
+extension DetailCoinView {
+    func setLayout() {
+        
     }
 }
